36db19207e4d4fcdcec62b41356ee2efb579bfdd
Felipe Jezus de Souza <felipejzsouza@gmail.com>
2017-12-23 17:34:42 -0200

    Inclusão de projeto com o pattern Bridge - Estrutural

---
+++ Bridge/Exportacao.cs
@@ -0,0 +1,17 @@
+﻿namespace Bridge
+{
+    public abstract class Exportacao
+    {
+        protected ExportacaoImpl _implementor;
+
+        public ExportacaoImpl Implementor
+        {
+            set { _implementor = value; }
+        }
+
+        public virtual void Exportar()
+        {
+            _implementor.Exportar();
+        }
+    }
+}
