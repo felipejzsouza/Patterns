af3eea994cafc0cceac71cdc45aea48043c04745
felipejzsouza <felipejzsouza@gmail.com>
2018-09-06 11:14:12 -0300

    adidionando projetos

---
+++ Observer/Classes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    public abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+        
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var o in _observers)
+            {
+                o.Update();
+            }
+        }
+    }
+    
+    public class Venda : Observer
+    {
+        private bool _state;
+
+        private Balanco _balanco;
+
+        public Venda(Balanco subject)
+        {
+            this._balanco = subject;
+        }
+
+        public void Iniciar()
+        {
+            if (!_state)
+                Console.WriteLine("Venda iniciada...");
+            else
+                Console.WriteLine("Não é possivel iniciar venda...");
+        }
+
+        public override void Update()
+        {
+            _state = _balanco.State;
+        }
+    }
+
+    public class Balanco : Subject
+    {
+        private bool _state = false;
+
+        public bool State
+        {
+            get { return _state; }
+        }
+
+        public void Iniciar()
+        {
+            _state = true; 
+            Console.WriteLine("Balanço iniciado...");
+            Notify();
+        }
+
+        public void Finalizar()
+        {
+            _state = false;
+            Console.WriteLine("Balanço finalizado...");
+            Notify();
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Balanco balanco = new Balanco();
+            Venda venda = new Venda(balanco);
+
+            balanco.Attach(venda);
+
+            balanco.Iniciar();
+            venda.Iniciar();
+            balanco.Finalizar();
+            venda.Iniciar();
+            Console.ReadLine();
+        }
+    }
+}
